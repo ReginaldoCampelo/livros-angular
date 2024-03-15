22cee6430fed9fbf6e32b900157c85963890ba85
Reginaldo Campelo <regin.developer@gmail.com>
2024-03-15 19:06:01 -0300

    improving new book component

--- src/app/components/new-book/new-book.component.ts
+++ src/app/components/new-book/new-book.component.ts
@@ -1,10 +1,116 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Book } from 'src/app/models/book.entity';
+import { BookService } from 'src/app/services/book.service';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-new-book',
   templateUrl: './new-book.component.html',
-  styleUrls: ['./new-book.component.scss']
+  styleUrls: ['./new-book.component.scss'],
 })
 export class NewBookComponent {
+  livroForm: FormGroup;
+  limitString: number = 65;
+  editoras: string[] = [];
+  autores: string[] = [];
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog,
+    private router: Router,
+    private dataService: BookService
+  ) {
+    this.livroForm = this.formBuilder.group({
+      titulo: ['', Validators.required],
+      resumo: ['', Validators.required],
+      editora: ['', Validators.required],
+      autores: ['', Validators.required],
+    });
+
+    this.dataService
+      .getEditoras()
+      .subscribe((editoras) => (this.editoras = editoras));
+    this.dataService
+      .getAutores()
+      .subscribe((autores) => (this.autores = autores));
+  }
+
+  onSubmit() {
+    if (this.livroForm.valid) {
+      let currentId: number = 0;
+      this.dataService.getBooks().subscribe((books) => {
+        currentId = books.length + 1;
+
+        const autoresValue = this.livroForm.value.autores;
+        const autores =
+          typeof autoresValue === 'string'
+            ? autoresValue.split(',')
+            : autoresValue;
+
+        const newBook: Book = {
+          id: currentId,
+          titulo: this.livroForm.value.titulo,
+          resumo: this.livroForm.value.resumo,
+          editora: this.livroForm.value.editora,
+          autores: autores,
+        };
+
+        this.dataService.addBook(newBook);
+
+        this.snackBar.open('Livro cadastrado com sucesso!', 'Fechar', {
+          duration: 3000,
+          verticalPosition: 'top',
+        });
+
+        const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+          width: '250px',
+          data: 'Deseja cadastrar outro livro?',
+        });
+
+        dialogRef.afterClosed().subscribe((result) => {
+          if (result) {
+            this.livroForm.reset();
+            this.resetValidation();
+          } else {
+            this.router.navigate(['/list']);
+          }
+        });
+      });
+    } else {
+      this.snackBar.open(
+        'Por favor, preencha todos os campos obrigatórios.',
+        'Fechar',
+        {
+          duration: 8500,
+          verticalPosition: 'top',
+        }
+      );
+    }
+  }
+
+  resetValidation() {
+    Object.keys(this.livroForm.controls).forEach((key) => {
+      this.livroForm.get(key)?.setErrors(null);
+    });
+  }
+
+  isInvalidAndTouched(fieldName: string): boolean {
+    const formControl = this.livroForm.get(fieldName);
+    return !!formControl?.invalid && !!formControl?.touched;
+  }
+
+  updateResumoCount(event: Event) {
+    const resumoControl = this.livroForm.get('resumo');
+    if (resumoControl) {
+      const resumoValue = (event.target as HTMLTextAreaElement).value;
+      resumoControl.setValue(resumoValue.substring(0, this.limitString), {
+        emitEvent: false,
+      });
+    }
+  }
 }
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,23 +1,76 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Book } from '../models/book.entity';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
+  private booksSubject: BehaviorSubject<Book[]> = new BehaviorSubject<Book[]>(
+    []
+  );
+  books$: Observable<Book[]> = this.booksSubject.asObservable();
 
-  constructor() { }
+  constructor() {
+    this.getBooks().subscribe((books) => this.booksSubject.next(books));
+  }
 
   getBooks(): Observable<Book[]> {
     const livros = [
-      { id: 1, titulo: 'Livro 1', resumo: 'Resumo do Livro 1', editora: 'Editora A', autores: ['Autor 1', 'Autor 2'] },
-      { id: 2, titulo: 'Livro 2', resumo: 'Resumo do Livro 2', editora: 'Editora B', autores: ['Autor 3'] },
-      { id: 3, titulo: 'Livro 3', resumo: 'Resumo do Livro 3', editora: 'Editora C', autores: ['Autor 4', 'Autor 5', 'Autor 6'] },
-      { id: 4, titulo: 'Livro 4', resumo: 'Resumo do Livro 4', editora: 'Editora D', autores: ['Autor 7'] },
-      { id: 5, titulo: 'Livro 5', resumo: 'Resumo do Livro 5', editora: 'Editora E', autores: ['Autor 8', 'Autor 9'] }
+      {
+        id: 1,
+        titulo: 'Livro 1',
+        resumo: 'Resumo do Livro 1',
+        editora: 'Editora A',
+        autores: ['Autor 1', 'Autor 2'],
+      },
+      {
+        id: 2,
+        titulo: 'Livro 2',
+        resumo: 'Resumo do Livro 2',
+        editora: 'Editora B',
+        autores: ['Autor 3'],
+      },
+      {
+        id: 3,
+        titulo: 'Livro 3',
+        resumo: 'Resumo do Livro 3',
+        editora: 'Editora C',
+        autores: ['Autor 4', 'Autor 5', 'Autor 6'],
+      }
     ];
 
     return of(livros);
+  }
+
+  getEditoras(): Observable<string[]> {
+    const editoras = [
+      'Editora 1',
+      'Editora 2',
+      'Editora 3',
+      'Editora 4',
+      'Editora 5',
+    ];
+    return of(editoras);
+  }
+
+  getAutores(): Observable<string[]> {
+    const autores = [
+      'Autor 1',
+      'Autor 2',
+      'Autor 3',
+      'Autor 4',
+      'Autor 5',
+      'Autor 6',
+      'Autor 7',
+      'Autor 8',
+    ];
+    return of(autores);
+  }
+
+  addBook(book: Book): void {
+    const currentBooks = this.booksSubject.getValue();
+    const updatedBooks = [...currentBooks, book];
+    this.booksSubject.next(updatedBooks);
   }
 }
