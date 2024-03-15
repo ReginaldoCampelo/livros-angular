afac1b9542ec085c3b547cb085729a5f590f10a7
Reginaldo Campelo <regin.developer@gmail.com>
2024-03-15 17:36:59 -0300

    adding router for new book component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListBookComponent } from './components/list-book/list-book.component';
+import { NewBookComponent } from './components/new-book/new-book.component';
 
 const routes: Routes = [
   { path: 'list', component: ListBookComponent },
+  { path: 'new', component: NewBookComponent },
   { path: '**', redirectTo: 'list' }
 ];
 
