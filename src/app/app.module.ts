9d4f23b973b1af22ef7ee1c3043eb595685cdc63
Reginaldo Campelo <regin.developer@gmail.com>
2024-03-15 17:35:15 -0300

    adding list book component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { ListBookComponent } from './components/list-book/list-book.component';
 
 const MATERIAL_MODULES = [
   MatSidenavModule,
@@ -22,7 +23,8 @@ const MATERIAL_MODULES = [
 @NgModule({
   declarations: [
     AppComponent,
-    SidenavComponent
+    SidenavComponent,
+    ListBookComponent
   ],
   imports: [
     BrowserModule,
